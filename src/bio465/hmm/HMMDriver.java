907aee52a99b02a76977aa01de658499cef682d8
jergason <jergason@2b898f66-1366-11df-8b85-374ad9ff7b16>
2010-02-19 01:02:20 +0000

    finished my own HMM method, and am now working on supervized training

--- src/bio465/hmm/HMMDriver.java
+++ src/bio465/hmm/HMMDriver.java
@@ -5,6 +5,7 @@ public class HMMDriver {
 	public static void main(String[] args) {
 		String pathToFastaFile = args[0];
 		HMM markovModel = new HMM(pathToFastaFile);
-		markovModel.calculateHiddenState();
+		String hiddenState = markovModel.calcHiddenState();
+		System.out.println(hiddenState);
 	}
 }
