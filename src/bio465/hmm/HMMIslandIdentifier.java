8456251871131336031f5e87747b11b145b10e3a
jergason <jergason@2b898f66-1366-11df-8b85-374ad9ff7b16>
2010-02-19 03:35:16 +0000

    now will store a list of all island states

--- src/bio465/hmm/HMMIslandIdentifier.java
+++ src/bio465/hmm/HMMIslandIdentifier.java
@@ -1,12 +1,16 @@
 package bio465.hmm;
+import java.util.List;
+import java.util.LinkedList;
 
 public class HMMIslandIdentifier {
 	private int windowSize = 0;
 	private double threshold = 0;
+	private List<Island> islandStates;
 	
 	public HMMIslandIdentifier(int windowSize, double threshold) {
 		this.windowSize = windowSize;
 		this.threshold = threshold;
+		islandStates = new LinkedList<Island>();
 	}
 	
 	/*
@@ -18,13 +22,52 @@ public class HMMIslandIdentifier {
 	public void identifyCpGIslands(String state) {
 		String window;
 		int ICount;
+		double CpGRatio;
 		for (int i = 0; i < state.length(); i++) {
 			window = state.substring(i, i + windowSize);
 			ICount = window.replaceAll("B", "").length();
-			if ((double)(ICount/windowSize) > threshold) {
+			CpGRatio = (double)ICount / windowSize;
+			if (CpGRatio > threshold) {
 				//store a state object in a list containing the number of islands, and the percentage
 				//@TODO: mark this region as a state
+				islandStates.add(new Island(CpGRatio, i, i + windowSize));
 			}
+		}
+	}
+	
+	private class Island {
+		public double CpGRatio = 0;
+		public int startOfIsland = 0;
+		public int endOfIsland = 0;
+		
+		public Island(double CpG, int start, int end) {
+			CpGRatio = CpG;
+			startOfIsland = start;
+			endOfIsland = end;
+		}
+
+		public double getCpGRatio() {
+			return CpGRatio;
+		}
+
+		public void setCpGRatio(double cpGRatio) {
+			CpGRatio = cpGRatio;
+		}
+
+		public int getStartOfIsland() {
+			return startOfIsland;
+		}
+
+		public void setStartOfIsland(int startOfIsland) {
+			this.startOfIsland = startOfIsland;
+		}
+
+		public int getEndOfIsland() {
+			return endOfIsland;
+		}
+
+		public void setEndOfIsland(int endOfIsland) {
+			this.endOfIsland = endOfIsland;
 		}
 	}
 }
