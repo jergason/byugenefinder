520489f1019663b0e9997228a3531dcda50a8ce6
jergason <jergason@2b898f66-1366-11df-8b85-374ad9ff7b16>
2010-02-19 03:40:34 +0000

    more basic updates

--- src/bio465/hmm/HMMDriver.java
+++ src/bio465/hmm/HMMDriver.java
@@ -1,5 +1,6 @@
 package bio465.hmm;
 import bio465.hmm.HMM;
+import bio465.hmm.HMMIslandIdentifier;
 
 public class HMMDriver {
 	public static void main(String[] args) {
@@ -7,6 +8,8 @@ public class HMMDriver {
 		String pathToParams = args[1];
 		HMM markovModel = new HMM(pathToFastaFile, pathToParams);
 		String hiddenState = markovModel.calcHiddenState();
+		HMMIslandIdentifier islandIdentifier = new HMMIslandIdentifier(200, .65);
+		islandIdentifier.identifyCpGIslands(hiddenState);
 		System.out.println(hiddenState);
 	}
 }
