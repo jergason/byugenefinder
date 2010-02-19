6e1ca5e89a46ccf63d34096005fd5e611cd2250a
jergason <jergason@2b898f66-1366-11df-8b85-374ad9ff7b16>
2010-02-19 15:57:33 +0000

    addin Island class

---
+++ trunk/src/bio465/hmm/Island.java
@@ -0,0 +1,31 @@
+package bio465.hmm;
+
+public class Island {
+	public double islandToNonIslandRatio = 0;
+	public int startOfIsland = 0;
+	public int endOfIsland = 0;
+	
+	public Island(double iToNon, int start, int end) {
+		islandToNonIslandRatio = iToNon;
+		startOfIsland = start;
+		endOfIsland = end;
+	}
+
+	public double getIslandToNonIslandRatio() {
+		return islandToNonIslandRatio;
+	}
+
+	public int getStartOfIsland() {
+		return startOfIsland;
+	}
+
+	public int getEndOfIsland() {
+		return endOfIsland;
+	}
+	
+	public String toString() {
+		String toReturn = "";
+		toReturn += "<Island from " + startOfIsland + " to " + endOfIsland + " | " + islandToNonIslandRatio + "% Island state>";
+		return toReturn;
+	}
+}
